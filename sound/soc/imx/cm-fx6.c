28a5b3835d1cdb4ca6677d8406168961effa7ebd
wolfgar <stephan.rafin@laposte.net>
2013-11-16 23:55:01 +0100

    Compulab patch to fix analog sampling frequency

--- sound/soc/imx/cm-fx6.c
+++ sound/soc/imx/cm-fx6.c
@@ -58,13 +58,10 @@
 static int __devinit imx_wm8731_probe(struct platform_device *pdev);
 static int __devinit imx_wm8731_remove(struct platform_device *pdev);
 static int imx_wm8731_init(struct snd_soc_pcm_runtime *rtd);
-static int imx_hifi_startup_slv_mode(struct snd_pcm_substream *substream);
-static int imx_hifi_hw_params_slv_mode(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params);
-static int imx_hifi_startup_mst_mode(struct snd_pcm_substream *substream);
-static int imx_hifi_hw_params_mst_mode(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params);
+static int imx_hifi_startup(struct snd_pcm_substream *substream);
 static void imx_hifi_shutdown(struct snd_pcm_substream *substream);
+static int imx_hifi_hw_params(struct snd_pcm_substream *substream,
+							  struct snd_pcm_hw_params *params);
 
 
 
@@ -76,9 +73,9 @@ struct imx_priv {
 static struct imx_priv card_priv;
 
 static struct snd_soc_ops imx_hifi_ops = {
-	.startup		= imx_hifi_startup_slv_mode,
+	.startup		= imx_hifi_startup,
 	.shutdown		= imx_hifi_shutdown,
-	.hw_params		= imx_hifi_hw_params_slv_mode,
+	.hw_params		= imx_hifi_hw_params,
 };
 
 static struct snd_soc_dai_link imx_dai[] = {
@@ -133,7 +130,7 @@ static const struct snd_soc_dapm_route audio_map[] = {
 	{ "Mic Bias",		NULL,	"Mic Jack"},
 };
 
-static int imx_hifi_startup_slv_mode(struct snd_pcm_substream *substream)
+static int imx_hifi_startup(struct snd_pcm_substream *substream)
 {
 	/*
 	 * As SSI's sys clock rate depends on sampling rate,
@@ -143,8 +140,17 @@ static int imx_hifi_startup_slv_mode(struct snd_pcm_substream *substream)
 	return 0;
 }
 
-static int imx_hifi_hw_params_slv_mode(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
+static void imx_hifi_shutdown(struct snd_pcm_substream *substream)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *codec_dai = rtd->codec_dai;
+	struct mxc_audio_platform_data *plat = card_priv.pdev->dev.platform_data;
+
+	if (!codec_dai->active)
+		plat->clock_enable(0);
+}
+
+static int imx_hifi_hw_params(struct snd_pcm_substream *substream, struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
@@ -160,9 +166,9 @@ static int imx_hifi_hw_params_slv_mode(struct snd_pcm_substream *substream,
 
 	sampling_rate = params_rate(params);
 	channels = params_channels(params);
-	// pr_info("%s:%s  sampling rate = %u  channels = %u \n", __FUNCTION__,
-	// 	(substream->stream == SNDRV_PCM_STREAM_PLAYBACK ? "Playback" : "Capture"),
-	// 	sampling_rate, channels);
+	pr_debug("%s:%s  sampling rate = %u  channels = %u \n", __FUNCTION__,
+		 (substream->stream == SNDRV_PCM_STREAM_PLAYBACK ? "Playback" : "Capture"),
+		 sampling_rate, channels);
 
 	/* set CPU DAI configuration */
 	switch (sampling_rate)
@@ -196,20 +202,9 @@ static int imx_hifi_hw_params_slv_mode(struct snd_pcm_substream *substream,
 
 	/* set i.MX active slot mask */
 	/* S[TR]CCR:DC */
-	switch (channels)
-	{
-	case 2:
-		tx_mask = 0xfffffffc;
-		rx_mask = 0xfffffffc;
-		break;
-	case 1:
-		tx_mask = 0xfffffffe;
-		rx_mask = 0xfffffffe;
-		break;
-	default:
-		return -EINVAL;
-	}
-	snd_soc_dai_set_tdm_slot(cpu_dai, tx_mask, rx_mask, 2, 32);
+	tx_mask = ~((1 << channels) - 1);
+	rx_mask = tx_mask;
+	snd_soc_dai_set_tdm_slot(cpu_dai, tx_mask, rx_mask, channels, 32);
 
 
 	ret = snd_soc_dai_set_sysclk(cpu_dai,
@@ -221,60 +216,48 @@ static int imx_hifi_hw_params_slv_mode(struct snd_pcm_substream *substream,
 		return ret;
 	}
 
-
+	/*
+	 * SSI sysclk divider:
+	 * div_2:	/1 or /2
+	 * div_psr:	/1 or /8
+	 * div_pm:	/1 .. /256
+	 */
+	div_2	= 0;
+	div_psr	= 0;
 	switch (sampling_rate)
 	{
 	case 8000:
-		// 1, 1, 12
-		div_2	= 0;
-		div_psr	= 0;
+		/* 1x1x12 */
 		div_pm	= 11;
 		break;
 	case 32000:
-		// 1, 1, 3
-		div_2	= 0;
-		div_psr	= 0;
+		/* 1x1x3 */
 		div_pm	= 2;
 		break;
 	case 48000:
-		// 1, 1, 2
-		div_2	= 0;
-		div_psr	= 0;
+		/* 1x1x2 */
 		div_pm	= 1;
 		break;
 	case 96000:
-		// 1, 1, 1
-		div_2	= 0;
-		div_psr	= 0;
+		/* 1x1x1 */
 		div_pm	= 0;
 		break;
 	case 44100:
-		// 1, 1, 2
-		div_2	= 0;
-		div_psr	= 0;
+		/* 1x1x2 */
 		div_pm	= 1;
 		break;
 	case 88200:
-		// 1, 1, 1
-		div_2	= 0;
-		div_psr	= 0;
+		/* 1x1x1 */
 		div_pm	= 0;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK) {
-		snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_TX_DIV_2, (div_2 ? SSI_STCCR_DIV2 : 0));
-		snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_TX_DIV_PSR, (div_psr ? SSI_STCCR_PSR : 0));
-		snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_TX_DIV_PM, div_pm);
-	}
-	else {
-		// capture
-		snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_RX_DIV_2, (div_2 ? SSI_SRCCR_DIV2 : 0));
-		snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_RX_DIV_PSR, (div_psr ? SSI_SRCCR_PSR : 0));
-		snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_RX_DIV_PM, div_pm);
-	}
+	/* sync mode: a single clock controls both playback and capture */
+	snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_TX_DIV_2, (div_2 ? SSI_STCCR_DIV2 : 0));
+	snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_TX_DIV_PSR, (div_psr ? SSI_STCCR_PSR : 0));
+	snd_soc_dai_set_clkdiv(cpu_dai, IMX_SSI_TX_DIV_PM, div_pm);
 
 	/* set codec DAI configuration */
 	dai_format = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
@@ -284,118 +267,16 @@ static int imx_hifi_hw_params_slv_mode(struct snd_pcm_substream *substream,
 	if (ret < 0)
 		return ret;
 
-	/* in slave mode, all it does is mask 'inappropriate clock rates' warning in wm8731.c */
 	ret = snd_soc_dai_set_sysclk(codec_dai,
 				     WM8731_SYSCLK_MCLK,
 				     plat->sysclk,
 				     SND_SOC_CLOCK_IN);
 	if (ret < 0) {
-		pr_err("Failed to set codec master clock to %u: %d \n",
-		       plat->sysclk, ret);
+		pr_err("Failed to set codec master clock to %u: %d \n", plat->sysclk, ret);
 		return ret;
 	}
 
 	return 0;
-}
-
-
-static int imx_hifi_startup_mst_mode(struct snd_pcm_substream *substream)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	struct mxc_audio_platform_data *plat = card_priv.pdev->dev.platform_data;
-
-	if (!codec_dai->active)
-		plat->clock_enable(1);
-
-	return 0;
-}
-
-static int imx_hifi_hw_params_mst_mode(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	struct mxc_audio_platform_data *plat = card_priv.pdev->dev.platform_data;
-	u32 dai_format;
-	unsigned int channels;
-	unsigned int tx_mask, rx_mask;
-	unsigned int sampling_rate;
-	int ret;
-
-
-	sampling_rate = params_rate(params);
-	channels = params_channels(params);
-	// pr_info("%s:%s  sampling rate = %u  channels = %u \n", __FUNCTION__,
-	// 	(substream->stream == SNDRV_PCM_STREAM_PLAYBACK ? "Playback" : "Capture"),
-	// 	sampling_rate, channels);
-
-	/* set cpu DAI configuration */
-	dai_format = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_IF |
-		SND_SOC_DAIFMT_CBM_CFM;
-
-	ret = snd_soc_dai_set_fmt(cpu_dai, dai_format);
-	if (ret < 0)
-		return ret;
-
-	/* set i.MX active slot mask */
-	/* S[TR]CCR:DC */
-	switch (channels)
-	{
-	case 2:
-		tx_mask = 0xfffffffc;
-		rx_mask = 0xfffffffc;
-		break;
-	case 1:
-		tx_mask = 0xfffffffe;
-		rx_mask = 0xfffffffe;
-		break;
-	default:
-		return -EINVAL;
-	}
-	snd_soc_dai_set_tdm_slot(cpu_dai, tx_mask, rx_mask, 2, 32);
-
-	ret = snd_soc_dai_set_sysclk(cpu_dai,
-				     IMX_SSP_SYS_CLK,
-				     0/*internally ignored*/,
-				     SND_SOC_CLOCK_OUT);
-	if (ret < 0) {
-		pr_err("Failed to set SSI clock: %d \n", ret);
-		return ret;
-	}
-
-
-	/* set codec DAI configuration */
-	dai_format = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
-		SND_SOC_DAIFMT_CBM_CFM;
-
-	ret = snd_soc_dai_set_fmt(codec_dai, dai_format);
-	if (ret < 0)
-		return ret;
-
-	ret = snd_soc_dai_set_sysclk(codec_dai,
-				     WM8731_SYSCLK_MCLK,
-				     plat->sysclk,
-				     SND_SOC_CLOCK_IN);
-	if (ret < 0) {
-		pr_err("Failed to set codec master clock to %u: %d \n",
-		       plat->sysclk, ret);
-		return ret;
-	}
-
-	return 0;
-}
-
-
-static void imx_hifi_shutdown(struct snd_pcm_substream *substream)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	struct mxc_audio_platform_data *plat = card_priv.pdev->dev.platform_data;
-
-	if (!codec_dai->active)
-		plat->clock_enable(0);
 }
 
 static int imx_wm8731_init(struct snd_soc_pcm_runtime *rtd)
@@ -435,51 +316,48 @@ out_retcode:
 /**
  * Configure AUDMUX interconnection between 
  * _slave (CPU side) and _master (codec size)
+ * 
+ * When SSI operates in master mode, 5-wire interconnect with
+ * audio codec is required:
+ * TXC  - BCLK
+ * TXD  - DAC data
+ * RXD  - ADC data
+ * TXFS - {DAC|ADC}LRC, i.e. word clock
+ * RXC  - MCLK, i.e. oversampling clock
+ * Audmux is operated in asynchronous mode to enable 6-wire
+ * interface (as opposed to 4-wire interface in sync mode). 
  */
-static int imx_audmux_config_slv_mode(int _slave, int _master)
+static int imx_audmux_config(int _slave, int _master)
 {
 	unsigned int ptcr, pdcr;
 	int slave = _slave - 1;
 	int master = _master - 1;
 
-
-	ptcr = MXC_AUDMUX_V2_PTCR_SYN;
-	pdcr = MXC_AUDMUX_V2_PDCR_RXDSEL(master);
-	mxc_audmux_v2_configure_port(slave, ptcr, pdcr);
-
-	ptcr = MXC_AUDMUX_V2_PTCR_SYN;
-	ptcr |= MXC_AUDMUX_V2_PTCR_TFSDIR |
+	ptcr = MXC_AUDMUX_V2_PTCR_SYN |
+		MXC_AUDMUX_V2_PTCR_TFSDIR |
 		MXC_AUDMUX_V2_PTCR_TFSEL(slave) |
+		MXC_AUDMUX_V2_PTCR_RCLKDIR |
+		MXC_AUDMUX_V2_PTCR_RCSEL(slave | 0x8) |
 		MXC_AUDMUX_V2_PTCR_TCLKDIR |
 		MXC_AUDMUX_V2_PTCR_TCSEL(slave);
-	ptcr |= MXC_AUDMUX_V2_PTCR_RCLKDIR |
-		MXC_AUDMUX_V2_PTCR_RCSEL(8 | slave);
 
 	pdcr = MXC_AUDMUX_V2_PDCR_RXDSEL(slave);
 	mxc_audmux_v2_configure_port(master, ptcr, pdcr);
-
-	return 0;
-}
-
-static int imx_audmux_config_mst_mode(int _slave, int _master)
-{
-	unsigned int ptcr, pdcr;
-	int slave = _slave - 1;
-	int master = _master - 1;
+	ptcr = ptcr & ~MXC_AUDMUX_V2_PTCR_SYN;
+	mxc_audmux_v2_configure_port(master, ptcr, pdcr);
 
 
-	ptcr = MXC_AUDMUX_V2_PTCR_SYN;
-	ptcr |= MXC_AUDMUX_V2_PTCR_TFSDIR |
-		MXC_AUDMUX_V2_PTCR_TFSEL(master) |
+
+	ptcr = MXC_AUDMUX_V2_PTCR_SYN |
+		MXC_AUDMUX_V2_PTCR_RCLKDIR |
+		MXC_AUDMUX_V2_PTCR_RCSEL(master | 0x8) |
 		MXC_AUDMUX_V2_PTCR_TCLKDIR |
 		MXC_AUDMUX_V2_PTCR_TCSEL(master);
+
 	pdcr = MXC_AUDMUX_V2_PDCR_RXDSEL(master);
 	mxc_audmux_v2_configure_port(slave, ptcr, pdcr);
-
-
-	ptcr = MXC_AUDMUX_V2_PTCR_SYN;
-	pdcr = MXC_AUDMUX_V2_PDCR_RXDSEL(slave);
-	mxc_audmux_v2_configure_port(master, ptcr, pdcr);
+	ptcr = ptcr & ~MXC_AUDMUX_V2_PTCR_SYN;
+	mxc_audmux_v2_configure_port(slave, ptcr, pdcr);
 
 	return 0;
 }
@@ -494,15 +372,7 @@ static int __devinit imx_wm8731_probe(struct platform_device *pdev)
 
 	card_priv.pdev = pdev;
 
-	if (!strncmp("wm8731-mst-mode", plat->codec_name, 15)) {
-		/* override default settings */
-		imx_audmux_config_mst_mode(plat->src_port, plat->ext_port);
-		imx_hifi_ops.startup = imx_hifi_startup_mst_mode;
-		imx_hifi_ops.hw_params = imx_hifi_hw_params_mst_mode;
-	}
-	else {
-		imx_audmux_config_slv_mode(plat->src_port, plat->ext_port);
-	}
+	imx_audmux_config(plat->src_port, plat->ext_port);
 
 	if (plat->init && plat->init()) {
 		return -EINVAL;
